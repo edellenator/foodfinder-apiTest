8fefb20f94c0462479caa1f2d9748bae77e5edeb
clickity-clacking <130marissa.martinez@gmail.com>
2022-04-05 16:47:09 -0700

    psuedo code 4.5

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//variables
+
+//open to main page containing food selector generator that can function right away
+    //function retrieve data from storage if present
+
+    //event listener for button press to provide food option
+
+        //if food option clicked
+
+            //function display restaurant info
+        
+        //else 
+
+            //remove first suggestion from restaurant list 
+
+            //reset so they can press button again and get new food option
+//
+
+//an option from the main page takes you to a survey where you can fill out food prefs
+
+    //check list where you can select multiple items to select cuisine types
+
+    //check list where you can select multiple items to select price bracket
+
+    //more preference options to be added based on readily available yelp/food review site API attributes
+
+    //submit button
+
+        //filter base restaurant array to match criteria from form
+
+        //store in local storage
